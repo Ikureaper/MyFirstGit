b649adb665ee205b42c2db1c2420deb9d22f2f02
Ikureaper <Inuyasha1>
2015-11-09 18:03:30 -0600

    homework completed

--- app.ts
+++ app.ts
@@ -1,8 +1,5 @@
-'use strict';
-var x = 12;
-var y = 3
-var result = addNumber(x, y)
-function addNumber(val1, val2){ val1 = 12 val2 = 3
-    var oops = val1 + val2
-    return oops
-}
+var input = prompt("What is your favorite number?");
+    if (isNaN(input)){
+    alert (input + " is not a number")
+    else if input = prompt("What is your favorite number?");}
+    else {alert (parseInt(input) * 100);}
